9f4f3c8a00c6e87dad0c9edce183c398e30b8f93
Hitsar <vlex09@ya.ru>
2022-12-05 21:51:08 +0300

    Создал проект, и игру
    xeenm подредоктировать, и игру можно выкладывать

---
+++ 0DGame/Assets/Game.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void ShowAdv();
+    [DllImport("__Internal")]
+    private static extern void SetToLeaderboard(int value);
+
+
+    [SerializeField] private int _health;
+    [SerializeField] private float timer;
+
+    [SerializeField] private GameObject _diedMenu;
+    [SerializeField] private GameObject _startMenu;
+
+    [SerializeField] private GameObject _particle;
+
+    [SerializeField] private TextMeshProUGUI _textPoint;
+    [SerializeField] private TextMeshProUGUI _textPointRecord;
+
+    private SpriteRenderer _spriteRenderer;
+    private int _point;
+    private int _ = 1;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _startMenu.SetActive(true);
+        _textPointRecord.text = "Рекорд: " + Progress.Instance.PlayerInfo._point.ToString();
+        ShowAdv();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && _ > 0)
+        {
+            _startMenu.SetActive(false);
+            Instantiate(_particle, Input.mousePosition, Quaternion.identity);
+            StartCoroutine("color");
+            _ = 0;
+        }
+
+        if (Input.GetMouseButtonDown(0) && _spriteRenderer.color == Color.green)
+        {
+            Point();
+        }
+        else if (Input.GetMouseButtonDown(0) && _spriteRenderer.color != Color.green)
+        {
+            TakeDamage();
+        }
+    }
+    private void TakeDamage()
+    {
+        _health--;
+        if (_health <= 0)
+        {
+            ShowAdv();
+            if (Progress.Instance.PlayerInfo._point < _point)
+            {
+                Progress.Instance.PlayerInfo._point = _point;
+                SetToLeaderboard(Progress.Instance.PlayerInfo._point);
+                Progress.Instance.Save();
+            }
+            _diedMenu.SetActive(true);
+        }
+    }
+    private void Point()
+    {
+        _point++;
+        _textPoint.text = _point.ToString();
+        Instantiate(_particle, Input.mousePosition, Quaternion.identity);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private IEnumerator color()
+    {
+        while (true)
+        {
+            if (Random.Range(0, 10) > 0)
+                _spriteRenderer.color = new Color(Random.Range(0, 1.0f), Random.Range(0, 1.0f), Random.Range(0, 1.0f));
+            else
+                _spriteRenderer.color = new Color(0, 1, 0);
+
+            yield return new WaitForSeconds(timer);
+        }
+    }
+}
